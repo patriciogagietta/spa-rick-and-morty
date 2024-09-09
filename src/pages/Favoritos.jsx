6b7c9d8dc3a2612f3656c6ee7d46f570c55e2d3d
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-09-09 00:41:32 -0300

    pagina seccion de favoritos

---
+++ src/pages/Favoritos.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../componentes/Header';
+import { PersonajesFavoritosGrilla } from '../componentes/PersonajesFavoritosGrilla';
+
+export const Favoritos = () => {
+    return (
+        <>
+            <Header />
+            <PersonajesFavoritosGrilla />
+        </>
+    );
+};
