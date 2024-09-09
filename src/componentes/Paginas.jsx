585474f9ec8410c5154338e586b3944ea293cab8
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-09-09 14:32:03 -0300

    modificacion para que el componente se adapte a las paginas de personajes y de las ubicaciones

--- src/componentes/Paginas.jsx
+++ src/componentes/Paginas.jsx
@@ -1,22 +1,18 @@
-import iconIzqDesactivado from '../assets/iconos/icon-pagina-izquierda.svg'
-import iconDerechaActivado from '../assets/iconos/icon-pagina-derecha-desactivada.svg'
+import iconIzqDesactivado from '../assets/iconos/icon-pagina-izquierda.svg';
+import iconDerechaActivado from '../assets/iconos/icon-pagina-derecha-desactivada.svg';
 
-import { useProductos } from '../hooks/useProductos'
-
-export const Paginas = () => {
-
-    const { handlePaginaSiguiente, handlePaginaAnterior, page } = useProductos();
+export const Paginas = ({ handlePaginaSiguiente, handlePaginaAnterior, page, pageFinal}) => {
 
     return (
-        <div className='flex justify-between'>
+        <div className='flex justify-between mt-10'>
             <div></div>
             <div className='flex gap-3 items-center'>
                 <button onClick={handlePaginaAnterior}>
-                    <img className={` rounded cursor-pointer w-8 ${page === 1 ? 'bg-slate-400' : 'bg-slate-800'}`} src={iconIzqDesactivado} alt="Icono Pagina Anterior" />
+                    <img className={`rounded cursor-pointer w-8 ${page === 1 ? 'bg-slate-400' : 'bg-slate-800'}`} src={iconIzqDesactivado} alt="Icono Pagina Anterior" />
                 </button>
-                <p className='font-bold'> Pagina {page}</p>
+                <p className='font-bold'> Pagina {page} de {pageFinal}</p>
                 <button onClick={handlePaginaSiguiente}>
-                    <img className='bg-gray-800 rounded cursor-pointer w-8' src={iconDerechaActivado} alt="Icono Pagina Siguiente" />
+                    <img className={`rounded cursor-pointer w-8 ${page === pageFinal ? 'bg-slate-400' : 'bg-gray-800 '}`} src={iconDerechaActivado} alt="Icono Pagina Siguiente" />
                 </button>
             </div>
         </div>
