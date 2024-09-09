29674d1bf0ada2c7ce8440b8fbb3730d2a324e71
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-09-09 14:35:01 -0300

    add componentes pagina

--- src/componentes/ProductosGrilla.jsx
+++ src/componentes/ProductosGrilla.jsx
@@ -3,34 +3,43 @@ import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { Paginas } from './Paginas';
 
 export const ProductosGrilla = () => {
 
-    const { producto, handleAgregarFavorito, personajeIsFavorito } = useProductos(); 
+    const { producto, handleAgregarFavorito, personajeIsFavorito, handlePaginaSiguiente, handlePaginaAnterior, page, pageFinalPersonajes } = useProductos(); 
 
     if (!producto) 
         return (
-        <div className="text-center font-bold text-2xl mt-28">Loading...</div>
+        <div className="text-center font-bold text-2xl mt-28">Cargando...</div>
     );
 
     return (
-        <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 mt-10'>
-            {producto.map((p) => (
-                <article className='flex flex-col gap-3' key={p.id}>
-                    <Link to={`/${p.id}`}>        
-                        <img className='rounded-3xl duration-200 hover:scale-105 cursor-pointer' src={p.image} alt={`Imagen Personaje ${p.name}`} />
-                    </Link>    
-                    <div className='flex items-center justify-between px-2'>
-                        <h4 className='font-bold'>{p.name}</h4>
-                        <button className='hover:scale-105' onClick={() => handleAgregarFavorito(p.id)}>
-                            {personajeIsFavorito(p.id)
-                                ? <FontAwesomeIcon icon={faHeart} style={{ color: "#ff0000", }} />
-                                : <FontAwesomeIcon icon={faHeart} style={{ color: "#000000", }} />
-                            }
-                        </button>
-                    </div>
-                </article>
-            ))}
-        </div>
+        <>
+            <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4 mt-10'>
+                {producto.map((p) => (
+                    <article className='flex flex-col gap-3' key={p.id}>
+                        <Link to={`/${p.id}`}>        
+                            <img className='rounded-3xl duration-200 hover:scale-105 cursor-pointer' src={p.image} alt={`Imagen Personaje ${p.name}`} />
+                        </Link>    
+                        <div className='flex items-center justify-between px-2'>
+                            <h4 className='font-bold'>{p.name}</h4>
+                            <button className='hover:scale-105' onClick={() => handleAgregarFavorito(p.id)}>
+                                {personajeIsFavorito(p.id)
+                                    ? <FontAwesomeIcon icon={faHeart} style={{ color: "#ff0000", }} />
+                                    : <FontAwesomeIcon icon={faHeart} style={{ color: "#000000", }} />
+                                }
+                            </button>
+                        </div>
+                    </article>
+                ))}
+            </div>
+            <Paginas 
+                handlePaginaSiguiente={handlePaginaSiguiente}
+                handlePaginaAnterior={handlePaginaAnterior}
+                page={page}
+                pageFinal={pageFinalPersonajes}
+            />
+        </>
     );
 };
