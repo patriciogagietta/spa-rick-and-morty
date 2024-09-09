9857efddd4b131f672748e69bcae658b6a30b618
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-09-09 14:37:12 -0300

    fix

--- src/componentes/PersonajesFavoritosGrilla.jsx
+++ src/componentes/PersonajesFavoritosGrilla.jsx
@@ -12,21 +12,26 @@ export const PersonajesFavoritosGrilla = () => {
         personajesFavoritos.length === 0 ? (
             <div className="text-center font-bold text-2xl mt-28">No hay personajes agregados a favoritos</div>
         ) : (
-            <div className='grid grid-cols-6 gap-4 mt-10'>
-                {personajesFavoritos.map((p) => (
-                    <article className='flex flex-col gap-3' key={p.id}>
-                        <Link to={`/favoritos/${p.id}`}>
-                            <img className='rounded-3xl duration-200 hover:scale-105 cursor-pointer' src={p.image} alt={`Imagen Personaje ${p.name}`} />
-                        </Link>
-                        <div className='flex items-center justify-between px-2'>
-                            <h4 className='font-bold'>{p.name}</h4>
-                            <button className='hover:scale-105' onClick={() => handleAgregarFavorito(p.id)}>
-                                <FontAwesomeIcon icon={faHeart} style={{ color: "#f10e0e" }} />
-                            </button>
-                        </div>
-                    </article>
-                ))}
-            </div>
+            <>
+                <div>
+                    <h3 className="text-3xl font-bold mt-14">Personajes favoritos</h3>
+                </div>
+                <div className='grid grid-cols-6 gap-4 mt-6'>
+                    {personajesFavoritos.map((p) => (
+                        <article className='flex flex-col gap-3' key={p.id}>
+                            <Link to={`/favoritos/${p.id}`}>
+                                <img className='rounded-3xl duration-200 hover:scale-105 cursor-pointer' src={p.image} alt={`Imagen Personaje ${p.name}`} />
+                            </Link>
+                            <div className='flex items-center justify-between px-2'>
+                                <h4 className='font-bold'>{p.name}</h4>
+                                <button className='hover:scale-105' onClick={() => handleAgregarFavorito(p.id)}>
+                                    <FontAwesomeIcon icon={faHeart} style={{ color: "#f10e0e" }} />
+                                </button>
+                            </div>
+                        </article>
+                    ))}
+                </div>
+            </>
         )
     );
 };
