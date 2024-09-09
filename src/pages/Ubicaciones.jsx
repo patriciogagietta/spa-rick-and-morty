117f0dba0fb8284b6e8168265258bbca7330c425
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-09-09 00:42:54 -0300

    pagina seccion ubicaciones

---
+++ src/pages/Ubicaciones.jsx
@@ -0,0 +1,11 @@
+import { Header } from '../componentes/Header';
+import { LocacionesGrilla } from '../componentes/LocacionesGrilla';
+
+export const Ubicacion = () => {
+    return (
+        <>
+            <Header />
+            <LocacionesGrilla />
+        </>
+    );
+};
