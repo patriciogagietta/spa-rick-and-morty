dcd0172b9debba738c9ca74fae104eb870be74e3
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-09-09 00:42:08 -0300

    pagina detalles personajes

---
+++ src/pages/DetallesPersonaje.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../componentes/Header";
+import { InfoPersonaje } from "../componentes/InfoPersonaje";
+
+export const DetallesPersonaje = () => {
+    return (
+        <>
+            <Header />
+            <InfoPersonaje />
+        </>
+  );
+};
