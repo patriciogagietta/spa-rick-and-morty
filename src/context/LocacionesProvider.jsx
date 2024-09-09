a7f1fae52a5fbab9b8d4e2c8ffe52e313f9a2562
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-09-09 14:29:46 -0300

    add funcion para agregar ubicaciones a favoritos

--- src/context/LocacionesProvider.jsx
+++ src/context/LocacionesProvider.jsx
@@ -6,12 +6,16 @@ export const LocacionesContext = createContext();
 export const LocacionesProvider = ({ children }) => {
 
     const [locaciones, setLocaciones] = useState([]);
+    const [pageLocacion, setPageLocacion] = useState(1);
+    const [pageFinalLocacion, setPageFinalLocacion] = useState(1);
+    const [locacionesFavoritas, setLocacionesFavoritas] = useState([]);
 
     useEffect(() => {
         const obtenerLocaiones = async () => {
             try{
-                const results = await consultarApiLocation();
-                setLocaciones(results);
+                const data = await consultarApiLocation(pageLocacion);
+                setLocaciones(data.results);
+                setPageFinalLocacion(data.info.pages);
             }catch(error){
                 console.error("Error al obtener las locaiones: " ,error);
                 throw error;
@@ -19,12 +23,54 @@ export const LocacionesProvider = ({ children }) => {
         };
 
         obtenerLocaiones();
-    }, []);
+    }, [pageLocacion]);
+
+    const handlePaginaSiguienteLocacion = () => {
+        if (pageLocacion < pageFinalLocacion){
+            setPageLocacion(pageLocacion + 1);
+        }
+    };
+
+    const handlePaginaAnteriorLocacion = () => {
+        if (pageLocacion > 1){
+            setPageLocacion(pageLocacion - 1);
+        };
+    };
+
+    const handlePaginaIcicialLocacion = () => {
+        setPageLocacion(1);
+    };
+
+    const handleAgregarLocacionesFavoritas = (id) => {
+        const locacionFavorita = locaciones.find((l) => l.id === id);
+
+        const lIsFavorita = locacionIsFavorita(id);
+
+        if (lIsFavorita) {
+            const eliminarLocacionFavorita = locacionesFavoritas.filter((l) => l.id !== id);
+            setLocacionesFavoritas(eliminarLocacionFavorita)
+        } else if (locacionFavorita){
+            setLocacionesFavoritas([...locacionesFavoritas, locacionFavorita]);
+        };
+    };
+
+    const locacionIsFavorita = (id) => {
+        const favorito = locacionesFavoritas.some((l) => l.id === id);
+        return favorito;
+    };
 
 
     return(
         <LocacionesContext.Provider value={{
-                locaciones,
+            locaciones,
+            handlePaginaSiguienteLocacion,
+            handlePaginaAnteriorLocacion,
+            pageLocacion,
+            pageFinalLocacion,
+            handlePaginaIcicialLocacion,
+            locacionesFavoritas,
+            handleAgregarLocacionesFavoritas,
+            locacionIsFavorita,
             }}
         >
             {children};
