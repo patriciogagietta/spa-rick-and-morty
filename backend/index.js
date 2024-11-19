1fc00245e19d3ae5b3249dfc2abd937992f5f265
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-19 16:14:18 -0300

    fix error de variable mal tipeada

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 
+import userRouter from './routes/user.routes.js'
+
 const app = express();
 
 // middleware para capturar la request y detectar si tiene que hacer esa transformacion para acceder al req.body en el post
 app.use(express.json());
 
-app.use('/api/users', routes.u)
+app.use('/api/users', userRouter)
 
 const PORT = process.env.PORT || 3000;
 
