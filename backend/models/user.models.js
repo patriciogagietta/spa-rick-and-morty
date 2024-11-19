0284ccb77e2595ab54332f52f4dd35697a239f0a
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-19 16:15:29 -0300

    add funcion para encontrar user por email y por username

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,7 +1,6 @@
 import { db } from '../database/connection.database.js'
 
-const crateUser = async ({ username, email, password }) => {
-
+const createUser = async ({ username, email, password }) => {
     const query = {
         text: `
             INSERT INTO users (username, email, password)
@@ -19,4 +18,48 @@ const crateUser = async ({ username, email, password }) => {
         console.error('Error creating user:', error)
         throw error
     }
+}
+
+const findByEmail = async (email) => {
+    const query = {
+        text: `
+            SELECT * FROM users
+            WHERE email = $1
+        `,
+        values: [email]
+    }
+
+    try {
+        const { rows } = await db.query(query);
+        return rows[0]
+
+    } catch (error) {
+        console.error('Error find user by email:', error)
+        throw error
+    }    
+}
+
+const findByUsername = async (username) => {
+    const query = {
+        text: `
+            SELECT * FROM users
+            WHERE username = $1
+        `,
+        values: [username]
+    }
+
+    try {
+        const { rows } = await db.query(query);
+        return rows[0]
+
+    } catch (error) {
+        console.error('Error find user by username:', error)
+        throw error
+    }
+}
+
+export const UserModel = {
+    createUser,
+    findByEmail,
+    findByUsername
 }
