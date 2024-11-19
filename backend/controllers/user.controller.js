cc82edefe4299adf0408e11d7073be7125cff08b
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-19 16:17:01 -0300

    creación del controller para gestionar usuarios

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,88 @@
+import { UserModel } from "../models/user.models.js"
+import { UserSchema } from "../schema/user.schema.js"
+
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+const register = async(req, res) => {
+    try {
+        const result = UserSchema.validateUser(req.body)
+
+        if (result.error) {
+            return res.status(400).json({ ok: false, msg: result.error.issues })
+        }
+
+        const { username, email, password } = req.body
+
+        const userUsername = await UserModel.findByUsername(username)
+        if (userUsername) {
+            return res.starus(409).json({ ok: false, msg: 'Username already exists' })
+        }
+
+        const userEmail = await UserModel.findByEmail(email)
+        if (userEmail) {
+            return res.status(409).json({ ok: false, msg: 'Email already exists'})
+        }
+
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
+        const newUser = await UserModel.createUser({ username, email, password: hashedPassword })
+
+        const token = jwt.sign({username: newUser.username, email: newUser.email},
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h"
+            }
+        )
+
+        return res.status(201).json({ ok: true, msg: token})
+    } catch {
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error server'
+        })
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        const result = UserSchema.validateLogin(req.body)
+
+        if (result.error) {
+            return res.status(400).json({ ok: false, msg: result.error.issues })
+        }
+
+        const { email, password } = req.body
+
+        const user = await UserModel.findByEmail(email)
+        if (!user) {
+            return res.status(404).json({ ok: false, msg: 'Email not exists'})
+        }
+
+        const isMatch = await bcryptjs.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(401).json({ ok: false, msg: 'Invalid password'})
+        }
+
+        const token = jwt.sign({ email: user.email },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h"
+            }
+        )
+
+        return res.status(200).json( {ok: true, msg: token})
+
+    } catch {
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error server',
+        })
+    }
+}
+
+export const UserController = {
+    register,
+    login
+}
