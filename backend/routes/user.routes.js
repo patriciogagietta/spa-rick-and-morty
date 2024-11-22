1359e26ba0fef76547a5a16a376c4ae2f39e784c
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-22 19:42:27 -0300

    add middleware para que verifique que el usuario haya iniciado sesion para cerrar sesion

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller.js";
+import { verifyToken } from "../middlewares/jwt.middleware.js";
 
 const router = Router();
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
-router.post('/logout', UserController.logout)
+router.post('/logout', verifyToken, UserController.logout)
 
 export default router;
