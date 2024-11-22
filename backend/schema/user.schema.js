59106752cd6e4a900c59d17f978d6744a238139d
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-22 19:45:21 -0300

    mejora en los manejos de errores

--- backend/schema/user.schema.js
+++ backend/schema/user.schema.js
@@ -2,23 +2,23 @@ import { z } from "zod";
 
 const userSchema = z.object({
     username: z
-        .string()
+        .string({ required_error: "El username es requerido"})
         .min(3, { message: "El nombre de usuario debe tener al menos 3 caracteres" })
         .max(20, { message: "El nombre de usuario no puede tener mas de 20 caracteres" }),
     email: z
-        .string()
+        .string({ required_error: "El email es requerido"})
         .email({ message: "Debe ser un correo electronico valido" }),
     password: z
-        .string()
+        .string({ required_error: "La contraseña es requerida"})
         .min(8, { message: "La contraseña debe tener al menos 8 caracteres" })
 });
 
 const loginSchema = z.object({
     email: z
-        .string()
+        .string({ required_error: "El email es requerido"})
         .email({ message: "Debe ser un correo electrónico valido" }),
     password: z
-        .string()
+        .string({ required_error: "La contraseña es requerida"})
 });
 
 const validateUser = (object) => {
