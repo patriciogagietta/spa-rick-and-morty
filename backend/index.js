96b73d2d083e8f87bcf0d08e77ff819fbfa15d45
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-23 02:22:48 -0300

    app use cors para solucionar el problema de cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import userRouter from './routes/user.routes.js'
 import favoritesRouter from './routes/personaje.favorite.routes.js'
 
 const app = express();
 
+app.use(cors())
 // middleware para capturar la request y detectar si tiene que hacer esa transformacion para acceder al req.body en el post
 app.use(express.json());
 // middleware para leer las cookies
