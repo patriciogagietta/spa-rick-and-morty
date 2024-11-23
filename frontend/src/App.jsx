5e26211f1b719dc34be6608e1daa232835d8a730
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-23 02:30:17 -0300

    add componente User provider y toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,33 @@
 import { Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 import { ProductosProvider } from './context/ProductosProvider';
 import { Inicio } from './pages/Inicio';
 import { Favoritos } from './pages/Favoritos';
 import { InicioSesion } from './pages/InicioSesion';
 import { Registro } from './pages/Registro';
 import { DetallesPersonaje } from './pages/DetallesPersonaje';
+import { UserProvider } from './context/UserProvider';
 
 function App() {
 
   return (
-    <ProductosProvider>
-      <div className='max-w-7xl mx-auto mb-20'>
-        <Routes>
-          <Route path='/' element={<Inicio />}/>
-          <Route path='/favoritos' element={<Favoritos />} />
-          <Route path='/iniciosesion' element={<InicioSesion />} />
-          <Route path='/registro' element={<Registro />} />
-          <Route path='/:id' element={<DetallesPersonaje />}/>
-          <Route path='/favoritos/:id' element={<DetallesPersonaje />} />
-        </Routes>
-      </div>
-    </ProductosProvider>
+    <UserProvider>
+      <ProductosProvider>
+        <div className='max-w-7xl mx-auto mb-20'>
+          <Routes>
+            <Route path='/' element={<Inicio />}/>
+            <Route path='/favoritos' element={<Favoritos />} />
+            <Route path='/iniciosesion' element={<InicioSesion />} />
+            <Route path='/registro' element={<Registro />} />
+            <Route path='/:id' element={<DetallesPersonaje />}/>
+            <Route path='/favoritos/:id' element={<DetallesPersonaje />} />
+          </Routes>
+        </div>
+        <ToastContainer />
+      </ProductosProvider>
+    </UserProvider>
   );
 };
 
