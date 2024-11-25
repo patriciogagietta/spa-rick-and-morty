a9c3e3359670c29a532cda093997db6f967f377c
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-25 00:12:57 -0300

    page para una ruta incorrecta

---
+++ frontend/src/pages/Error404.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../componentes/Header";
+import RutaIncorrecta from "../componentes/RutaIncorrecta";
+
+export const Error404 = () => {
+    return (
+        <>
+            <Header />
+            <RutaIncorrecta />
+        </>
+    )
+};
