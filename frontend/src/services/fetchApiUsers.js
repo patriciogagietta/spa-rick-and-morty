59ab9f14ca9936928b7fe73f483b524043b0b1fd
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-25 00:13:48 -0300

    consultas a la api backend de users

--- frontend/src/services/fetchApiUsers.js
+++ frontend/src/services/fetchApiUsers.js
@@ -1,13 +1,41 @@
-import axios from 'axios'
-
-const API_USERS = 'http://localhost:3000/api/users'
+import axios from './axios'
 
 export const consultarRegistroUser = async (user) => {
     try {
-        const response = await axios.post(`${API_USERS}/register`, user)
+        const response = await axios.post(`/users/register`, user)
         return response.data
     } catch (error) {
         console.error('Error al consultar la api register: ', error);
         throw error; 
+    }
+}
+
+export const consultarLoginUser = async (user) => {
+    try {
+        const response = await axios.post(`/users/login`, user)
+        return response.data
+    } catch (error) {
+        console.error('Error al consultar la api login: ', error);
+        throw error;
+    }
+}
+
+export const consultarLogoutUser = async () => {
+    try {
+        const response = await axios.post('/users/logout')
+        return response.data
+    } catch (error) {
+        console.error('Error al consultar la api logout: ', error);
+        throw error;
+    }
+}
+
+export const consultarVerifyUser = async () => {
+    try {
+        const response = await axios.get('/users/verify')
+        return response.data
+    } catch (error) {
+        console.error('Error al consultar la api verify user: ', error);
+        throw error;
     }
 }
