e6694b917c0da46925d5bb154bc198fc1a498ce3
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-25 00:18:28 -0300

    add muestra si inicio sesion o muestra para cerrar sesion

--- frontend/src/componentes/Header.jsx
+++ frontend/src/componentes/Header.jsx
@@ -2,10 +2,13 @@ import { Link } from 'react-router-dom';
 
 import logoHeader from '../assets/logo-header.webp';
 import { useProductos } from '../hooks/useProductos';
+import { useUser } from '../hooks/useUser';
+import { ModalLogout } from './ModalLogout';
 
 export const Header = () => {
 
   const { handlePaginaInicio } = useProductos();
+  const { isAutenticado, modalVisible, handleLogout } = useUser()
 
   return (
     <header className='flex  justify-between items-center mt-4'>
@@ -18,7 +21,14 @@ export const Header = () => {
       <ul className='flex gap-10'>
         <Link to='/' onClick={handlePaginaInicio}> <p className='font-bold hover:scale-105'>Inicio</p></Link>
         <Link to='/favoritos'><p className='font-bold hover:scale-105'>Favoritos</p></Link>
-        <Link to='/iniciosesion'><p className='font-bold hover:scale-105'>Inicio Sesion</p> </Link>
+        {isAutenticado ? (
+          <>
+            <button onClick={handleLogout} className='font-bold hover:scale-105'>Cerrar Sesion</button>
+            {modalVisible && <ModalLogout />}
+          </>
+        ) : (
+          <Link to='/iniciosesion'><p className='font-bold hover:scale-105'>Iniciar Sesion</p> </Link>
+        )}
       </ul>
     </header>
   );
