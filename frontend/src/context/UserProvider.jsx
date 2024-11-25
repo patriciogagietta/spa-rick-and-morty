d0fd9a623011e90a1e2a8c3e4ab0ef3ab8f9ed4c
patriciogagietta1 <patoxxgagietta1@gmail.com>
2024-11-25 00:16:35 -0300

    add funciones para consultar a la base de datos y validar la autenticacion

--- frontend/src/context/UserProvider.jsx
+++ frontend/src/context/UserProvider.jsx
@@ -1,7 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { toast, Zoom } from 'react-toastify';
+import Cookies from 'js-cookie';
 
-import { consultarRegistroUser } from "../services/fetchApiUsers";
+import { consultarRegistroUser, consultarLoginUser, consultarVerifyUser, consultarLogoutUser } from "../services/fetchApiUsers";
 
 export const UserContext = createContext()
 
@@ -9,6 +10,7 @@ export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [isAutenticado, setIsAutenticado] = useState(false)
     const [errors, setErrors] = useState(null)
+    const [modalVisible, setModalVisible] = useState(false)
 
     const registerUser = async (user) => {
         try {
@@ -20,7 +22,6 @@ export const UserProvider = ({ children }) => {
                 autoClose: 1500,
                 hideProgressBar: false,
                 closeOnClick: true,
-                pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
                 theme: "light",
@@ -38,12 +39,104 @@ export const UserProvider = ({ children }) => {
         }
     }
 
+    const loginUser = async (user) => {
+        try {
+            const response = await consultarLoginUser(user)
+            setUser(response.user)
+            setIsAutenticado(true)
+            toast.success('Sesion iniciada correctamente', {
+                position: "bottom-left",
+                autoClose: 1500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Zoom
+            });
+        } catch (error) {
+            if (error.response.status === 400) {
+                setErrors(error.response.data.msg[0].message)
+            } else if (error.response.status === 409) {
+                setErrors(error.response.data.msg)
+            } else {
+                setErrors("Error server")
+            }
+        }
+    }
+
+    const logoutUser = async () => {
+        try {
+            await consultarLogoutUser()
+            setUser(null)
+            setIsAutenticado(false)
+            setModalVisible(false)
+            toast.success('Sesion cerrada correctamente', {
+                position: "bottom-left",
+                autoClose: 1500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Zoom
+            });
+                        
+        } catch {
+            setErrors('Error al cerrar sesión');
+        }
+    }
+
+    // limpiar el error cada 3 seg y si cambia de pestaña se hace el clearTimeout
+    useEffect(() => {
+        const time = setTimeout(() => {
+            setErrors(null)
+        }, 3000)
+        return () => clearTimeout(time)
+    }, [errors])
+
+    // obtener cookies cuando se renderiza la app para saber si un usuario esta logueado
+    useEffect(() => {
+        const verifyUser = async () => {
+            const cookies = Cookies.get()
+
+            if (cookies.token) {
+                try {
+                    const response = await consultarVerifyUser(cookies.token)
+                    if (!response.user) {
+                        return setIsAutenticado(false)
+                    }
+                    setIsAutenticado(true)
+                    setUser(response.user)
+
+                } catch {
+                    setIsAutenticado(false)
+                    setUser(null)
+                }
+            }
+        }
+        verifyUser()
+    }, [])
+
+    const handleLogout = () => {
+        setModalVisible(true)
+    }
+
+    const handleLogoutCancel = () => {
+        setModalVisible(false)
+    }
+
     return (
         <UserContext.Provider value={{
             user,
             registerUser,
             isAutenticado,
-            errors
+            errors,
+            loginUser,
+            logoutUser,
+            modalVisible,
+            handleLogout,
+            handleLogoutCancel
         }}>
             {children}
         </UserContext.Provider>
